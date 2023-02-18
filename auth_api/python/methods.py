1e637bf010871d03f66d06007055ebfae2601d79
Guille <ggarza31416@gmail.com>
2023-02-18 01:39:53 -0600

    add the code for the Token class

--- auth_api/python/methods.py
+++ auth_api/python/methods.py
@@ -1,9 +1,46 @@
-# These functions need to be implemented
+import mysql.connector
+import json
+import jwt
+import os
+
 class Token:
+    def __init__(self):
+        # initiating the db connection by using env variables.
+        self.connection = mysql.connector.connect(
+            user=os.environ['DB_USER'],
+            password=os.environ['DB_PASSWORD'],
+            host=os.environ['DB_HOST'],
+            port=os.environ['DB_PORT'],
+            database=os.environ['DB_DATABASE']
+        )
+
+    def generate_jwt(self, db_role):
+        payload = {"role": db_role}
+        key = os.environ['JWT_KEY']
+        algorithm = "HS256"
+        token = jwt.encode(payload, key, algorithm=algorithm)
+        return token
 
     def generate_token(self, username, password):
-        return 'test'
+         # verifying the user and the plain password exist in the database. using the sha2 function to derive the encrypted password in the DB side.
+        query = "SELECT * FROM users WHERE username = %s AND password = SHA2(CONCAT(%s, salt), 512)"
+        values = (username, password)
 
+        try:
+            with self.connection.cursor() as cursor:
+                cursor.execute(query, values)
+                record = cursor.fetchone()
+        # verifying i got at least one record to generate the jwt token response.
+            if record:
+                db_role = record[3]
+                jwt_token = self.generate_jwt(db_role)
+                return jwt_token
+        # returning a 403 error message if the password and username dont exist in the DB as requested in the task.
+            return json.dumps({'error': 'Forbidden'}), 403, {'Content-Type': 'application/json'}
+        
+        except Exception as e:
+            # raising an exception in case there is a an error in the server.
+            return json.dumps({'error': 'Internal Server Error'}), 500, {'Content-Type': 'application/json'}
 
 class Restricted:
 
