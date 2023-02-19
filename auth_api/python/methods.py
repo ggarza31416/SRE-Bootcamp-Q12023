71a80adf42d5720b1138acaa0b53fc3c8aadb683
Guille <ggarza31416@gmail.com>
2023-02-18 18:34:11 -0600

    update Token class and sytle in PEP 8 format

--- auth_api/python/methods.py
+++ auth_api/python/methods.py
@@ -1,7 +1,7 @@
 import mysql.connector
-import json
 import jwt
 import os
+
 
 class Token:
     def __init__(self):
@@ -22,7 +22,8 @@ class Token:
         return token
 
     def generate_token(self, username, password):
-         # verifying the user and the plain password exist in the database. using the sha2 function to derive the encrypted password in the DB side.
+        # verifying the user and the plain password exist in the database.
+        # using the sha2 function to derive the encrypted password in the DB side
         query = "SELECT * FROM users WHERE username = %s AND password = SHA2(CONCAT(%s, salt), 512)"
         values = (username, password)
 
@@ -30,19 +31,18 @@ class Token:
             with self.connection.cursor() as cursor:
                 cursor.execute(query, values)
                 record = cursor.fetchone()
-        # verifying i got at least one record to generate the jwt token response.
-            if record:
-                db_role = record[3]
-                jwt_token = self.generate_jwt(db_role)
-                return jwt_token
-        # returning a 403 error message if the password and username dont exist in the DB as requested in the task.
-            return json.dumps({'error': 'Forbidden'}), 403, {'Content-Type': 'application/json'}
-        
-        except Exception as e:
-            # raising an exception in case there is a an error in the server.
-            return json.dumps({'error': 'Internal Server Error'}), 500, {'Content-Type': 'application/json'}
+                # verifying i got at least one record to generate the jwt token response
+                if record:
+                    db_role = record[3]
+                    jwt_token = self.generate_jwt(db_role)
+                    return jwt_token
+                # returning a 403 error message if password and username dont exist in the DB
+                return 403
+        except Exception:
+            # raising an exception in case there is a an error in the server
+            return 500
+
 
 class Restricted:
-
     def access_data(self, authorization):
         return 'test'
