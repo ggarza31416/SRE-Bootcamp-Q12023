c502894cfc1d87b0a84ff45917ce5726d40e23bf
Guille <ggarza31416@gmail.com>
2023-02-18 18:42:08 -0600

    add code for API endpoints

--- auth_api/python/api.py
+++ auth_api/python/api.py
@@ -1,6 +1,4 @@
-from flask import Flask
-from flask import jsonify
-from flask import request
+from flask import Flask, jsonify, request
 from methods import Token, Restricted
 
 app = Flask(__name__)
@@ -23,22 +21,35 @@ def url_health():
 # e.g. http://127.0.0.1:8000/login
 @app.route("/login", methods=['POST'])
 def url_login():
-    username = request.form['username']
-    password = request.form['password']
-    res = {
-        "data": login.generate_token(username, password)
-    }
-    return jsonify(res)
+    username = request.form.get('username')
+    password = request.form.get('password')
+    if not username or not password:
+        return jsonify({'error': 'Username and password are required.'}), 400
+    token = login.generate_token(username, password)
+    if token == 403:
+        return jsonify({'error': 'Invalid username or password.'}), 403
+    elif token == 500:
+        return jsonify({'error': 'Internal Server Error.'}), 500
+    return jsonify({'data': token})
 
 
 # # e.g. http://127.0.0.1:8000/protected
 @app.route("/protected")
 def url_protected():
-    auth_token = request.headers.get('Authorization')
-    res = {
-        "data": protected.access_data(auth_token)
-    }
-    return jsonify(res)
+    auth_header = request.headers.get('Authorization')
+    if not auth_header:
+        return jsonify({'error': 'Authorization header is missing.'}), 401
+
+    parts = auth_header.split()
+    if len(parts) != 2 or parts[0].lower() != 'bearer':
+        return jsonify({'error': 'Invalid Authorization header.'}), 401
+
+    token = parts[1]
+    data = protected.access_data(token)
+    if not data:
+        return jsonify({'error': 'Invalid or expired token.'}), 401
+
+    return jsonify({'data': data})
 
 
 if __name__ == '__main__':
